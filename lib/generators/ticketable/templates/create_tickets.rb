19c5a999e9573f66b3e11d04d1f1e2013ac89dd7
antho1404 <anthony.estebe@gmail.com>
2013-01-28 12:02:17 +0100

    add functionality to model

--- lib/generators/ticketable/templates/create_tickets.rb
+++ lib/generators/ticketable/templates/create_tickets.rb
@@ -2,12 +2,13 @@ class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
       t.references :<%= name.singularize.downcase %>
-      t.integer :status
+      t.integer :status, default: 0
       t.text :content
       t.references :ticketable, polymorphic: true
 
       t.timestamps
     end
     add_index :tickets, :<%= name.singularize.downcase %>_id
+    add_index :tickets, :status
   end
 end
--- lib/generators/ticketable/templates/ticket.rb
+++ lib/generators/ticketable/templates/ticket.rb
@@ -2,4 +2,17 @@ class Ticket < ActiveRecord::Base
   belongs_to :<%= name.singularize.downcase %>
   belongs_to :ticketable, polymorphic: true
   attr_accessible :status, :content, :ticketable
+  attr_accessible :<%= name.singularize.downcase %>_id
+
+  [:open, :in_progress, :close].each_with_index do |status, i|
+    define_method "is_#{status.to_s}?" do
+      self.status == i
+    end
+
+    define_method "#{status}!" do
+      self.update_attribute :status, i
+    end
+
+    scope status.to_s.pluralize.to_sym, -> { where(status: i) }
+  end
 end
